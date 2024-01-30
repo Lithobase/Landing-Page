6a1133a8dd71993b87aa7fca9c204db547671ca8
techadam90 <abdulrahman.adamu@receeve.com>
2024-01-30 08:27:33 +0100

    Implement responsive mobile menu and content & close menu when link item is clicked

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -2,9 +2,7 @@
   <div>
     <Container>
       <nav class="bg-white py-2">
-        <div
-          class="py-4 flex flex-wrap items-center justify-between"
-        >
+        <div class="py-4 flex items-center justify-between">
           <router-link
             to="/"
             class="flex items-center space-x-3 rtl:space-x-reverse"
@@ -21,7 +19,7 @@
           </router-link>
 
           <div
-            class="flex space-x-3 md:space-x-0 rtl:space-x-reverse"
+            class="menu-wrapper flex space-x-3 md:space-x-0 rtl:space-x-reverse"
           >
             <div class="items-center justify-between w-full flex">
               <ul
@@ -29,7 +27,7 @@
               >
                 <li>
                   <a
-                    href="#"
+                    href="/"
                     class="block py-2 px-3 rounded md:bg-transparent text-blue-700 md:p-0"
                     >Home</a
                   >
@@ -37,11 +35,11 @@
                 <li class="relative">
                   <a
                     href="#"
-                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    class="flex flex-nowrap items-center space-x-1 py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
                     id="dropdownHoverButton"
                     data-dropdown-toggle="dropdownHover"
                     data-dropdown-trigger="hover"
-                    >Products
+                    ><span>Products</span>
                     <span class="las la-angle-down"></span>
                   </a>
 
@@ -55,25 +53,24 @@
                       aria-labelledby="dropdownHoverButton"
                     >
                       <li>
-                        <router-link
-                          to="/products/PpinaBox"
-                          href="#"
+                        <a
+                          href="/products/PpinaBox"
                           class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                           >Payment Processor
-                        </router-link>
+                        </a>
                       </li>
                       <li>
-                        <router-link
-                          to="/products/mifos"
+                        <a
+                          href="/products/mifos"
                           class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                          >Lithobase Core</router-link
+                          >Lithobase Core</a
                         >
                       </li>
                       <li>
-                        <router-link
-                          to="/products/search-app"
+                        <a
+                          href="/products/search-app"
                           class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                          >Payment Device</router-link
+                          >Payment Device</a
                         >
                       </li>
                     </ul>
@@ -81,33 +78,30 @@
                 </li>
                 <li>
                   <a
-                    href="#"
+                    href="/"
                     class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                     >Services</a
                   >
                 </li>
                 <li>
-                  <router-link
-                    to="/docs/overview"
-                    href="#"
+                  <a
+                    href="/docs/overview"
                     class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                    >Developers</router-link
+                    >Developers</a
                   >
                 </li>
                 <li>
                   <a
-                    href="#"
+                    href="/"
                     class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                     >Contact</a
                   >
                 </li>
               </ul>
             </div>
-
-            
           </div>
 
-          <div>
+          <div class="">
             <button
               class="hidden lg:inline-flex items-center space-x-4 rounded-full px-5 py-2 pr-3 bg-[#f5f5f5]"
             >
@@ -122,7 +116,8 @@
             </button>
 
             <button
-              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              class="inline-flex lg:hidden items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              @click="openMenu = !openMenu"
             >
               <span class="sr-only">Open main menu</span>
               <svg
@@ -141,6 +136,88 @@
                 />
               </svg>
             </button>
+
+            <div
+              :class="[
+                'mobile-menu w-full bg-white border-b border-t fixed top-20 left-0',
+                openMenu ? 'block' : 'hidden',
+              ]"
+            >
+              <ul
+                class="p-4 font-medium rounded-lg w-full px-[24px] md:px-0 md:w-[82vw] mx-auto"
+              >
+                <li>
+                  <a
+                    href="/"
+                    class="block py-2 px-3 rounded md:bg-transparent text-blue-700"
+                    >Home</a
+                  >
+                </li>
+                <li class="relative">
+                  <a
+                    class="block py-2 px-3 text-gray-900 rounded md:hover:bg-transparent md:hover:text-blue-700"
+                    id="dropdownDefaultButton"
+                    data-dropdown-toggle="dropdown"
+                    >Products
+                    <span class="las la-angle-down"></span>
+                  </a>
+
+                  <!-- Dropdown menu -->
+                  <div
+                    id="dropdown"
+                    class="z-20 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+                  >
+                    <ul
+                      class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                      aria-labelledby="dropdownHoverButton"
+                    >
+                      <li>
+                        <a
+                          href="/products/PpinaBox"
+                          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          >Payment Processor
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="/products/mifos"
+                          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          >Lithobase Core</a
+                        >
+                      </li>
+                      <li>
+                        <a
+                          href="/products/search-app"
+                          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          >Payment Device</a
+                        >
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    >Services</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/docs/overview"
+                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    >Developers</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    >Contact</a
+                  >
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
@@ -149,5 +226,16 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import Container from "@/components/partials/Container.vue";
+
+const openMenu = ref(false);
 </script>
+
+<style lang="scss" scoped>
+@media only screen and (max-width: 1024px) {
+  .menu-wrapper {
+    display: none;
+  }
+}
+</style>
